60dd3f6d5f1ac2842fc17d415e19848767e4cfe9
Andre Diniz <andre_fcd@yahoo.com.br>
2022-01-09 22:37:43 -0300

    primeiro commit

---
+++ hello.go
@@ -0,0 +1,150 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+const monitoramento = 3
+const delayEmSegundos = 5
+
+func main() {
+
+	exibeIntroducao()
+
+	for {
+		exibeMenu()
+		comando := lerComando()
+
+		switch comando {
+		case 1:
+			iniciarMonitoramento()
+		case 2:
+			fmt.Println("Exibindo Logs...")
+			imprimeLogs()
+		case 0:
+			fmt.Println("Saindo do programa")
+			os.Exit(0)
+		default:
+			fmt.Println("Não conheço esse comando!")
+			os.Exit(-1)
+		}
+	}
+}
+
+func exibeIntroducao() {
+	nome := "André"
+	versao := 1.1
+
+	fmt.Println("Olá mundo!", nome)
+	fmt.Println("A versão do sistema que você está rodadndo é", versao)
+
+}
+
+func exibeMenu() {
+	fmt.Println("1 - Iniciar Monitoramento")
+	fmt.Println("2 - Exibir Logs")
+	fmt.Println("0 - Sair do Programa")
+}
+
+func lerComando() int {
+	var comandoLido int
+	fmt.Scan(&comandoLido)
+	fmt.Println("O comando escolhido foi:", comandoLido)
+	fmt.Println()
+
+	return comandoLido
+}
+
+func iniciarMonitoramento() {
+	fmt.Println("Monitorando...")
+	sites := leSitesDoArquivo()
+
+	for i := 0; i < monitoramento; i++ {
+		for i, sites := range sites {
+			fmt.Println("Testando site: ", i+1, " : ", sites)
+			testaSite(sites)
+			fmt.Println()
+		}
+		fmt.Println("Realizando novo monitoramento em 5 segundos: ")
+		time.Sleep(delayEmSegundos * time.Second)
+	}
+
+	fmt.Println()
+}
+
+func testaSite(site string) {
+	resp, err := http.Get(site)
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro:", err)
+	}
+
+	if resp.StatusCode == 200 {
+		fmt.Println("Site:", site, "foi carregado com sucesso!")
+		registraLog(site, true)
+	} else {
+		fmt.Println("Site:", site, "está com problemas. Status Code:", resp.StatusCode)
+		registraLog(site, false)
+	}
+}
+
+func leSitesDoArquivo() []string {
+
+	var sites []string
+
+	arquivo, err := os.Open("sites.txt")
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro:", err)
+	}
+
+	leitor := bufio.NewReader(arquivo)
+	for {
+
+		linha, err := leitor.ReadString('\n')
+		linha = strings.TrimSpace(linha)
+		fmt.Println(linha)
+
+		sites = append(sites, linha)
+
+		if err == io.EOF {
+			break
+		}
+	}
+
+	arquivo.Close()
+
+	return sites
+}
+
+func registraLog(site string, status bool) {
+
+	arquivo, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro: ", err)
+	}
+
+	arquivo.WriteString(time.Now().Format("02/01/2006 15:04:05") + " - " + site + " - online: " + strconv.FormatBool(status) + "\n")
+
+	arquivo.Close()
+}
+
+func imprimeLogs() {
+
+	arquivo, err := ioutil.ReadFile("log.txt")
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro: ", err)
+	}
+
+	fmt.Println(string(arquivo))
+}
